fc8345cef34b0e12afad0ff1d72484176e62fd2c
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2026-01-21 00:11:50 +0500

    Res completed

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -18,17 +18,17 @@ function Intro() {
         <div className="w-full md:w-1/3">
           <Button text="Shop Now" />
         </div>
-        <div className="flex flex-wrap md:gap-10 mt-5 justify-between md:justify-start">
+        <div className="flex md:flex-wrap md:gap-10 mt-5 justify-between md:justify-start">
           <div className=" text-center border border-transparent pr-5  border-r-gray-500">
-            <h1 className="font-medium text-sm md:text-2xl">200+</h1>
+            <h1 className="font-medium text-xs md:text-2xl">200+</h1>
             <p className="text-gray-500 text-xs">International Brands</p>
           </div>
           <div className=" text-center border border-transparent pr-5  border-r-gray-500">
-            <h1 className="font-medium text-sm md:text-2xl">2000+</h1>
+            <h1 className="font-medium text-xs md:text-2xl">2000+</h1>
             <p className="text-gray-500 text-xs">High Quality Products</p>
           </div>
           <div className="text-center">
-            <h1 className="font-medium text-sm md:text-2xl">30,000+</h1>
+            <h1 className="font-medium text-xs md:text-2xl">30,000+</h1>
             <p className="text-gray-500 text-xs">Happy Customers</p>
           </div>
         </div>
